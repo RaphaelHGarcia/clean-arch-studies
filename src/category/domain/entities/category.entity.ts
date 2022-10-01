7637dfcf508f3f82506d80941938b2613dd8cdc7
Raphael Garcia <raphael@systemhope.com.br>
2022-10-01 19:45:19 -0300

    init repo

--- src/category/domain/entities/category.entity.ts
+++ src/category/domain/entities/category.entity.ts
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from 'uuid'
 
 export type CategoryEntityProps = {
   name: string
@@ -9,15 +10,29 @@ export type CategoryEntityProps = {
 
 export class CategoryEntity {
 
-  constructor(public readonly props: CategoryEntityProps) {
-    this.props.is_active = true
+  constructor(
+    public readonly props: CategoryEntityProps,
+    public id?: string
+  ) {
+    this.id = id || uuidv4()
+    this.description = this.props.description
+    this.props.is_active = this.is_active ?? true
+    this.props.created_at = this.created_at ?? new Date()
   }
 
   get name () { return this.props.name }
 
   get description () { return this.props.description }
 
+  private set description (value: string) {
+    this.props.description = value ?? null
+  }
+
   get is_active () { return this.props.is_active }
+
+  private set is_active (value: boolean) {
+    this.props.is_active = value ?? true
+  }
 
   get created_at () { return this.props.created_at }
 
--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -1,24 +1,83 @@
-import { CategoryEntity, CategoryEntityProps } from './category.entity'
+import { omit } from 'lodash'
+import { validate as uuidValidate } from 'uuid'
+import { CategoryEntity } from './category.entity'
 
 describe('Category Unit Tests', () => {
 
   test('constructor of Category', () => {
-    const created_at = new Date()
-    const props: CategoryEntityProps = {
+    let category = new CategoryEntity({ name: 'Movie' })
+    const categoryWithoutDesc = omit(category.props, 'created_at')
+    expect(categoryWithoutDesc).toStrictEqual({
       name: 'Movie',
-      description: 'desc',
+      description: null,
       is_active: true,
-      created_at
-    }
-
-    const category = new CategoryEntity(props)
-
-    expect(category.props).toStrictEqual({
-      name: 'Movie',
-      description: 'desc',
-      is_active: true,
-      created_at
     })
+
+    category = new CategoryEntity({ name: 'Movie', description: 'my desc' })
+    const categoryWithDesc = omit(category.props, 'created_at')
+    expect(categoryWithDesc).toStrictEqual({
+      name: 'Movie',
+      description: 'my desc',
+      is_active: true,
+    })
+
+    category = new CategoryEntity({ name: 'Movie', is_active: false })
+    const categoryWithActive = omit(category.props, 'created_at')
+    expect(categoryWithActive).toStrictEqual({
+      name: 'Movie',
+      description: null,
+      is_active: false,
+    })
+
+    category = new CategoryEntity({ name: 'Movie' })
+    expect(category.props.created_at).toBeInstanceOf(Date)
   })
 
+  test('id field', () => {
+    let category = new CategoryEntity({ name: 'Movie' })
+    expect(category.id).not.toBeNull()
+    expect(uuidValidate(category.id)).toBeTruthy()
+  })
+
+  test('getter of name prop', () => {
+    const category = new CategoryEntity({ name: 'Movie' })
+    expect(category.name).toBe('Movie')
+  })
+
+  test('getter and setter of description prop', () => {
+    let category = new CategoryEntity({ name: 'Movie' })
+    expect(category.description).toBeNull()
+
+    category = new CategoryEntity({ name: 'Movie', description: 'My description' })
+    expect(category.description).toBe('My description')
+
+    category['description'] = 'Desc updated'
+    expect(category.description).toBe('Desc updated')
+
+    category['description'] = undefined
+    expect(category.description).toBeNull()
+  })
+
+  test('getter and setter of is_active prop', () => {
+    let category = new CategoryEntity({ name: 'Movie' })
+    expect(category.is_active).toBeTruthy()
+
+    category = new CategoryEntity({ name: 'Movie', is_active: false })
+    expect(category.is_active).toBeFalsy()
+
+    category['is_active'] = undefined
+    expect(category.is_active).toBeTruthy()
+
+    category['is_active'] = false
+    expect(category.is_active).toBeFalsy()
+  })
+
+  test('getter of created_at prop', () => {
+    let category = new CategoryEntity({ name: 'Movie' })
+    expect(category.created_at).toBeInstanceOf(Date)
+
+    const created_at = new Date()
+    category = new CategoryEntity({ name: 'Movie', created_at })
+    expect(category.created_at).toBe(created_at)
+  })
 })
